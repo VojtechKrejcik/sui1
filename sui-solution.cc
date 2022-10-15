edd9ee00c478e5ac3a91078bf9d663f2fa9c4021
Vojtech Krejcik <vojtech.krejcik@gmail.com>
2022-10-15 15:46:22 +0200

    Its working but eating too much memory and its ugly

--- sui-solution.cc
+++ sui-solution.cc
@@ -22,6 +22,8 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 	SearchState working_state(init_state);
 	std::map<SearchState,std::tuple<SearchState,SearchAction>> backtrackmap;
 
+	std::vector<SearchState> states;
+
 	open.push(working_state);
 
 	backtrackmap.insert({working_state,{working_state,working_state.actions().front()}});
@@ -39,47 +41,36 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 			std::vector<SearchAction> solution;
 			SearchState* child_state = &cur_state;
 			SearchState* parent_state = &std::get<0>(backtrackmap.at(cur_state));
-			while (state_to_string(*child_state) != state_to_string(*parent_state))
+			while (true)
 			{
 				solution.insert(solution.begin(),std::get<1>(backtrackmap.at(*child_state)));
-				std::cout<<"Akce přidána: " << std::get<1>(backtrackmap.at(*child_state)) << std::endl;
 				child_state = parent_state;
-				std::cout<<*child_state<<std::endl;
+
+				if(state_to_string(*child_state) == state_to_string(working_state))
+				{
+					return solution;
+				}
 				parent_state = &std::get<0>(backtrackmap.at(*child_state));
 			}
-			std::cout<<"Solution ################################################################\n";
 			return solution;
 		}
 
 		if(closed.find(state_to_string(cur_state)) != closed.end())
-		{
 			continue;
-		}
 
 		auto actions = cur_state.actions();
 
 		if (actions.size() == 0)
-		{
-			// dead end
 			continue;
-		}
-
 		
 		for (int i=0; i < actions.size();i++)
 		{
-			//Add new states to open
 			open.push(actions[i].execute(cur_state));
-			//Add new states to map
 			backtrackmap.insert({actions[i].execute(cur_state),{cur_state,actions[i]}});
 		}
 		
-		//Close state and remove it from queue
-		//std::cout<<cur_state;
 		closed.insert(state_to_string(cur_state));
-		
-
 	}
-
 	return {};
 }
 
